91d1629bc181fb28b3ccd4b377114722df309923
Micah Cooper <micah@mrmicahcooper.com>
2018-02-08 10:24:12 -0600

    User views list of icons

--- lib/micon_web/controllers/page_controller.ex
+++ lib/micon_web/controllers/page_controller.ex
@@ -2,11 +2,18 @@ defmodule MiconWeb.PageController do
   use MiconWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    svgs = Micon.Svg.all()
+
+    conn
+    |> render("index.html", svgs: svgs)
   end
 
   def show(conn, _params) do
     path = conn.request_path
-    send_resp(conn, 200, path)
+    svgs = Micon.Svg.get(path)
+
+    conn
+    |> put_resp_content_type("image/svg+xml")
+    |> render("show.svg", svgs: svgs)
   end
 end
--- test/micon_web/controllers/page_controller_test.exs
+++ test/micon_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MiconWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Micon"
   end
 end
